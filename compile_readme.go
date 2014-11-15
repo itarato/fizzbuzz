6563953eccbdde0baed14f4509470a51fcd166b3
Peter Arato <it.arato@gmail.com>
2014-11-15 01:03:22 +0000

    Add compile script

---
+++ compile_readme.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
+
+var path = "./"
+
+func main() {
+	fmt.Println("FizzBuzz implementations")
+	fmt.Println("========================\n")
+
+	file, _ := os.Open(path)
+	dirInfos, _ := file.Readdir(0)
+	for _, dirInfo := range dirInfos {
+		if !dirInfo.IsDir() || dirInfo.Name() == ".git" {
+			continue
+		}
+
+		fmt.Println("#Language " + dirInfo.Name() + "\n")
+
+		languageFolder, _ := os.Open(path + dirInfo.Name() + "/")
+		languageFileList, _ := languageFolder.Readdir(0)
+		for _, languageFileInfo := range languageFileList {
+			regexp, _ := regexp.Compile(".*\\.class")
+			if regexp.MatchString(languageFileInfo.Name()) {
+				continue
+			}
+
+			fileContentBuff, _ := ioutil.ReadFile(path + dirInfo.Name() + "/" + languageFileInfo.Name())
+			content := string(fileContentBuff)
+
+			fmt.Println("```" + dirInfo.Name())
+			fmt.Println(content)
+			fmt.Println("```\n")
+		}
+	}
+
+}
